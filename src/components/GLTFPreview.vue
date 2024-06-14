04404e21a735d43ad9200d4f6a9bab8c289eb04a
王博文 <1363931801@qq.com>
2024-06-14 17:21:52 +0800

    feat:load 3d model

---
+++ src/components/GLTFPreview.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width: 1200px; height: 700px">
+    <model-viewer
+      id="model-viewer"
+      :src="MODEL"
+      ar
+      ar-modes="scene-viewer quick-look"
+      camera-controls
+      touch-action="pan-y"
+      alt="A 3D model of an astronaut"
+      shadow-intensity="2"
+      auto-rotate
+      disable-pan
+      skybox-height="2m"
+      max-camera-orbit="auto 90deg auto"
+    >
+    </model-viewer>
+  </div>
+</template>
+<script setup>
+import MODEL from '../assets/ali.glb'
+</script>
+<style>
+model-viewer {
+  position: relative;
+  background: transparent;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        // 添加以下内容
+        compilerOptions: {
+          isCustomElement: (tag) => ['model-viewer'].includes(tag),
+        },
+      },
+    }),
+  ],
+  server: {
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  assetsInclude: ['./src/assets/**/*.glb'],
+})
